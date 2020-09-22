7289f7ebde37e8f1410ae95a53c50e78a30b44fd
David Buser <busdav@outlook.com>
2020-09-22 10:48:38 +0100

    Start signup route; add express-validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,40 @@
-import express from "express";
+/* 
+`{ Request, Response }`: needed to import that from the @types/express package. Reason: after inserting the express-validator
+middleware function, typescript couldn't do the type inference anymore for `req` and `res`, so threw an error. Therefore, we needed to put
+explicit type annotations. 
+*/
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hi there!");
-});
+/* 
+express-validator is a middleware - "in the middle" after request and before our callback function i.e. request handler. The way it works is: 
+It inspects, validates and sanitizes the email and password properties of the request object, and if there is an error, it will 
+append an error object to the request object. This error prop can then be called upon in our request handler, through `validationResult`. 
+*/
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password myst be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log("Creating a user...");
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
