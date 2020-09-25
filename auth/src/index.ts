c6169aa19ccc10b0461f8666e9f00304cd9a23a3
David Buser <busdav@outlook.com>
2020-09-25 16:59:27 +0100

    Set up depl for Mongodb and connect it to auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,6 +8,7 @@ i.e. here, route handlers, rather than having to use next and having to say e.g.
 We want to avoid that as `next` can be complex to understand. 
 */
 import "express-async-errors";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -30,6 +31,37 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+const start = async () => {
+  /*
+  We only put all of this into a function because some versions of Node (maybe the ones we're running in our image) do not yet support 
+  the `await` keyword at the top level i.e. outside of a function. So, we define it in a function, and then call it at the bottom of 
+  the file. 
+  */
+  /*
+  The connection URL usually takes the form of "mongodb://[DOMAIN]" (e.g. if running mongo locally: "mongodb://localhost"). 
+  Here of course, we need to connect to our MongoDB instance running in the pod that our depl created, or rather, 
+  to the clusterIP service belonging to that pod. We simply put the name of that service where we would normally put the domain, 
+  plus the port, plus the name of the actual database inside there that we want to connect to (will be created if non-existant), 
+  plus an options object.
+  */
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    /* 
+    The mongoose.connect function will throw an error if unsuccessful. In order to be able to catch that error, we'll wrap the whole thing
+    into a try catch block, and do a console.error passing on the actual error (`err`). 
+    */
+    console.error(err);
+  }
+  // If we make it past the try catch block, it is time for us to start listening to incoming traffic i.e. requests.
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
