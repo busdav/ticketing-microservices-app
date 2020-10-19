173b402c7ffd17fd17366a762b384e7298fa95d8
David Buser <busdav@outlook.com>
2020-10-19 08:06:24 +0100

    Complete signin route handler
    Start currentUser route handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+  // The below is TS shorthand for `if (!req.session || !req.session.jwt)`. We need the first check in order to "prove" to TS that we're
+  // addressing the case where req.session could be `null`.
+  if (!req.session?.jwt) {
+    return res.send({ currentuser: null });
+  }
+
+  const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,23 +1,57 @@
 import express, { Request, Response } from "express";
-// Remember: this is a method that's going to be used as a middleware, to validate incoming data on the body of this POST request. 
+// Remember: this is a method that's going to be used as a middleware, to validate incoming data on the body of this POST request.
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 
+import { Password } from "../services/password";
+import { User } from "../models/user";
 import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
-router.post("/api/users/signin", 
-[
-  body("email")
-    .isEmail()
-    .withMessage("Email must be valid"),
-  body("password")
-    .trim()
-    .notEmpty()
-    .withMessage("You must supply a password")
-], 
-validateRequest,
-(req: Request, res: Response) => {
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+    //  Store JWT on session object.
+    req.session = {
+      jwt: userJwt,
+    };
+
+    // Status 200 (rather than 201) as we're no longer creating a new user.
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
