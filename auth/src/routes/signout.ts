8f02270679635e25d9e57d0e69ff64df51ae7c6a
David Buser <busdav@outlook.com>
2020-10-20 08:05:35 +0100

    Implement signout

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,14 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("Hi there!");
+  /*
+  What does it mean to sign out a user? Essentially, the response will include a header that will tell the browser to dump all 
+  the information in the cookie, which will of course remove the JWT. 
+  cookie-session documentation tells us that to destroy a session, we just do `req.session = null`. And, of course we
+  still have to send back a response, so we just send back an empty object. 
+  */
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
