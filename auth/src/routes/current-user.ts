9016b82c7c95b469f7e982ab54f31a10ce6860a5
David Buser <busdav@outlook.com>
2020-10-22 15:58:29 +0100

    Implement currentUser and requireAuth middlewares

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,26 +1,12 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  /*
-  The below is TS shorthand for `if (!req.session || !req.session.jwt)`. We need the first check in order to "prove" to TS that we're
-  addressing the case where req.session could be `null`. So, this if statement says "If there is no req.session object, OR, 
-  if there is one but the `jwt` property is not defined, then...".
-  */
-  if (!req.session?.jwt) {
-    return res.send({ currentuser: null });
-  }
-
-  // If someone has altered the JWT, then `jwt.verify` will throw an error. We need to make sure to capture that error.
-  // -> try/catch
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentuser: null });
-  }
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  // We want to send back `null` if the currentUser is not defined, rather than `undefined` -> we make that explicit
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
