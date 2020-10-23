3905f6bc00ec3a8f3f525339b230fb1f33dda523
David Buser <busdav@outlook.com>
2020-10-23 09:35:42 +0100

    Create setup.ts for jest tests

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,33 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose, { mongo } from "mongoose";
+import { app } from "../app";
+
+// We need to define this outside of the scope of beforeAll, as we also need to reference it in afterAll
+let mongo: any;
+
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  // We tell mongoose to look at all the different connections that exist inside the `mongo` we defined above, established through
+  // the `mongoose.connect` command inside of the `beforeAll` hook, and delete all associated collections.
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  // We stop running the MongoMemoryServer
+  await mongo.stop();
+  // We tell mongoose to disconnect from it
+  await mongoose.connection.close();
+});
