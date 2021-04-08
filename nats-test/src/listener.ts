ee97c36c787796db1c666a61d3b0aeee0dc066da
David Buser <busdav@outlook.com>
2021-04-08 07:33:03 +0100

    Set manual ack mode

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,9 +10,17 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
+  // With most other JS libraries, you would expect to be able to pass in a configurations object, but not here:
+  // We chain the options on as additional method calls. One important one is: manual acknowledgment mode. This makes sure that we don't use the default behavior there.
+  // Default behavior: as soon as the library has received an event, it will tell the NATS streaming server "all good here" - but what if in the message handler,
+  // something goes wrong (e.g. connection to the DB was lost)? So, we need to be able to acknowledge manually.
+  // (If we don't acknowledge, then NATS will send the event again after e.g. 30s, or will send it to another member of the queue group)
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
   const subscription = stan.subscribe(
     "ticket:created",
-    "orders-service-queue-group"
+    "orders-service-queue-group",
+    options
   );
 
   // With most other librarier, we'd have this directly in a callback function to the first function above (`stan.subscribe`), but not with this library.
@@ -24,5 +32,7 @@ stan.on("connect", () => {
     if (typeof data === "string") {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
