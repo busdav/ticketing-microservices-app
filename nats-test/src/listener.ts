3dd9c5c9b521f36e362478b856683329375f03bf
David Buser <busdav@outlook.com>
2021-04-11 18:21:43 +0100

    Update listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,6 +10,14 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
+  // Even though we have the "heartbeat" healthchecks (see depl file), those checks generally take too long - we want to inform NATS more quickly of when a client
+  // is shutting down, so that NATS doesn't wait around trying to still send that client a (then delayed) message. So, we're writing event handlers
+  // that will tell NATS immediately that this client is about to shut down and should no longer be sent any messages. See console.log here and then handlers at the bottom.
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
   // With most other JS libraries, you would expect to be able to pass in a configurations object, but not here:
   // We chain the options on as additional method calls. One important one is: manual acknowledgment mode. This makes sure that we don't use the default behavior there.
   // Default behavior: as soon as the library has received an event, it will tell the NATS streaming server "all good here" - but what if in the message handler,
@@ -36,3 +44,7 @@ stan.on("connect", () => {
     msg.ack();
   });
 });
+
+// See above - these are event handlers that are watching
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
