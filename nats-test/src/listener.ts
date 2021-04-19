267ea2adaa893c14475f6e8f99d76fd2c0f46c1a
David Buser <busdav@outlook.com>
2021-04-19 07:58:58 +0100

    Set up stan further

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -22,12 +22,27 @@ stan.on("connect", () => {
   // We chain the options on as additional method calls. One important one is: manual acknowledgment mode. This makes sure that we don't use the default behavior there.
   // Default behavior: as soon as the library has received an event, it will tell the NATS streaming server "all good here" - but what if in the message handler,
   // something goes wrong (e.g. connection to the DB was lost)? So, we need to be able to acknowledge manually.
-  // (If we don't acknowledge, then NATS will send the event again after e.g. 30s, or will send it to another member of the queue group)
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  // If we don't acknowledge, then NATS will send the event again after e.g. 30s, or will send it to another member of the queue group.
+  // Queue groups avoid double processing of one event by several instances of the same microservice (in case you are scaling horizontally by adding multiple instances +
+  // load balancer, rather than vertically i.e. adding more compute power or memory) - NATS will only send an event to ONE member of a queue group at a time.
+  // Through making sure that every object created by a microservice has an ID and sequential versioning, we make sure that for a given object, events are not
+  // processed double, or out of order.
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    // setDeliverAllAvailable() is an option that sends ALL events that have ever been emitted to and by the NATS streaming server. We in principle need this in order to
+    // make sure that if the respective microservice ever goes down, it can catch up once it comes back online. However, if you imagine that you could have millions of events, this
+    // isn't feasible. So instead, we use setDurableName("<ID, i.e. serviceName>") that will create a durable subscription, i.e.,
+    // it will have NATS keep track of WHICH events have been successfully processed by the microservice (through acknowledgment) that was subscribed with the respective ID,
+    // and re-send ONLY THOSE that have not yet been marked as processed, once the microservice with that exact ID comes back online.
+    // Why do we still also need setDeliverAllAvailable()? For the very first time that the respective microservice comes online.
+    // The queue group below we also need for this, because it will make NATS maintain the durable subscription name even if the stan completely disconnects for some time.
+    .setDeliverAllAvailable()
+    .setDurableName("accounting-service");
 
   const subscription = stan.subscribe(
     "ticket:created",
-    "orders-service-queue-group",
+    "queue-group-name",
     options
   );
 
