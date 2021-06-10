1a39c3b0f04712837feebdf92f8304bf951b899c
David Buser <busdav@outlook.com>
2021-06-10 10:49:43 +0100

    Create initialized nats client (mirroring mongoose)

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
-
 import { app } from "./app";
+// Note the lower case `natsWrapper` - because we're importing an INSTANCE of the class, NOT the class itself, see comment in nats-wrapper.ts
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -11,6 +12,8 @@ const start = async () => {
   }
 
   try {
+    // The clusterId and url arguments of the natsWrapper.connect method refer back to our nats-deployment and respective service.
+    await natsWrapper.connect("ticketing", "asldkjefh", "http://nats-srv:4222");
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -1,5 +1,26 @@
 import nats, { Stan } from "node-nats-streaming";
 
-class NatsWrapper {}
+class NatsWrapper {
+  // If we don't put the '?' below, TS gives error: "Property '_client' has no initializer and is not definitely assigned in the constructor".
+  // But, we're running our constructor with `new NatsWrapper()` during export, and during that time it's too early to assign _client. We only want to do it
+  // once we initialize the nats client (i.e. the stan) in our index.ts file. So we put the `?` to tell TS that _client might be undefined for some periods of time.
+  private _client?: Stan;
 
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      this._client!.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
+      this._client!.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+// In order to replicate the OOTB behavior of e.g. mongoose (which nats does NOT have), we export an INSTANCE of the NatsWrapper class here, NOT the class itself.
+// Why? So that within the whole application, we then share the same natsWrapper instance THAT IS ALREADY CONNECTED (just like mongoose does it OOTB).
 export const natsWrapper = new NatsWrapper();
