1a7d4ea20c695e9c11b4d0e7936fc0afe71471e9
David Buser <busdav@outlook.com>
2021-07-19 08:27:24 +0100

    Create enum for order status

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
+import { OrderStatus } from "@db-udemy-microservices-ticketing/common";
 
 interface OrderAttrs {
   userId: string;
-  status: string;
+  // We originally had just the TS type "string", but danger is that if you ever create an event from any service with an order status that is a string,
+  // the possibility is super high that you'll make a typo and everything falls apart, so you want to restrict - across services - what an order status can be =>
+  // use an enum (which we defined in the common module)
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
@@ -27,6 +31,8 @@ const orderSchema = new mongoose.Schema(
     status: {
       type: String,
       required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
     },
     expiresAt: {
       type: mongoose.Schema.Types.Date,
