8ac406b2995b79f56dd872ee7629eeba4d8bc705
David Buser <busdav@outlook.com>
2021-07-21 09:21:16 +0100

    Add Ticketmodel to the Order microservice; start implementation

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+// We're not necessarily replicating ALL attributes of the ticket model from the Ticket microservice, as the Order microservice
+// only needs to know about the specific attributes below. That's also why we wouldn't put this model into the common module.
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
