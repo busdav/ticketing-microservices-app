0ed3ff0eb2075d45e0ea625d8df1202c83ed81ce
David Buser <busdav@outlook.com>
2021-09-03 08:57:54 +0200

    Continue implementation index test

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -3,6 +3,26 @@ import { app } from "../../app";
 import { Order } from "../../models/order";
 import { Ticket } from "../../models/ticket";
 
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+};
+
 it("fetches orders for a particular user", async () => {
   // Create three tickets
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+
+  // Create one order as User #1
+
+  // Create two orders as User #2
+  // Make request to get orders for User #2
+  // Make sure we only go tht orders for User #2
 });
